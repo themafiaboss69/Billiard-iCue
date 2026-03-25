afd75112ff2a17cf4f2ca8f8cfcd94779a890495
Mafia Boss <mafiaboss@MacBook-Pro.local>
2026-03-25 14:56:15 +0800

    Revert: Restored v15 stability due to black screen in v16

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'icue-guide-v16';
+const CACHE_NAME = 'icue-guide-v15';
 const urlsToCache = [
   './',
   './index.html',
-  './style.css?v=16',
-  './main.js?v=16',
+  './style.css?v=15',
+  './main.js?v=15',
   './manifest.json'
 ];
 
